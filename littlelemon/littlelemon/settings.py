de32fe765f4a07dd201b18e1017ae635176418a7
Luca Ionut-Catalin <112466341+IonutLuca86@users.noreply.github.com>
2025-01-28 16:57:20 +0100

    completed backend requirements

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurantAPI',
     'rest_framework',
-    
+    'rest_framework.authtoken',
+    'djoser',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+
+DJOSER = { "USER_ID_FIELD": "username" }
--- littlelemon/restaurantAPI/tests.py
+++ littlelemon/restaurantAPI/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Menu, Booking
+from .serializers import MenuItemSerializer
 
 # Create your tests here.
+
+class MenuItemTest(TestCase):
+    def test_string_representation(self):
+        item = Menu.objects.create(title="Test Item", price=10.00)
+        self.assertEqual(item, "Test Item : 10.00")
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        Menu.objects.create(title="Test Item 1", price=10.00, inventory=10)
+        Menu.objects.create(title="Test Item 2", price=20.00, inventory=20)
+        Menu.objects.create(title="Test Item 3", price=30.00, inventory=30)
+
+    def test_getall(self):
+        items = Menu.objects.all()
+        serializer = MenuItemSerializer(items, many=True)
+        self.assertEqual(serializer.data, [
+            {'id': items[0].id, 'title': 'Test Item 1', 'price': '10.00', 'inventory': 10},
+            {'id': items[1].id, 'title': 'Test Item 2', 'price': '20.00', 'inventory': 20},
+            {'id': items[2].id, 'title': 'Test Item 3', 'price': '30.00', 'inventory': 30},
+        ])
